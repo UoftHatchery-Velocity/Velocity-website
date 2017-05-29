6364e632a91c9a5cca94ec8439d01ad216d42cd5
Nuofan <nuofan.xu@uofthatchery.ca>
2017-05-29 00:11:40 -0400

    main page template almost finished

--- navbar.js
+++ navbar.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.navbar-fixed-top').css('background-color', 'rgba(34,34,34,0.9)');
+       } else {
+          $('.navbar-fixed-top').css('background-color', 'transparent');
+       }
+   });
+});
