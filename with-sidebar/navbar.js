4cc46da8c7bdc3dc4cbf4e5cf26ba606f355c035
Zhaocong Yuan <justin.yuan@mail.utoronto.ca>
2017-05-31 15:36:49 -0400

    fixed side nav bar and hamburger button

--- with-sidebar/navbar.js
+++ with-sidebar/navbar.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	$(window).on("scroll",function(){
   	var wn = $(window).scrollTop();
     if(wn > 600){
-    	$(".navbar").css("background","white").css("transition","0.4s ease-in-out ");
+    	$("#myTopnav").css("background","white").css("transition","0.4s ease-in-out ");
 		$(".navbar-default .navbar-nav > li > a ").css("color","black");
 		$(".navbar-default .navbar-nav > li > a ").css("opacity",".6");
     }
     else{
-    	$(".navbar").css("background","transparent").css("transition","0.4s ease-in-out ");
+    	$("#myTopnav").css("background","transparent").css("transition","0.4s ease-in-out ");
 		$(".navbar-default .navbar-nav > li > a ").css("color","#fcf8e3");
 		$(".navbar-default .navbar-nav > li > a ").css("opacity","1");
     }
