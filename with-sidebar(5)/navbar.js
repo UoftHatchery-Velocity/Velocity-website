f98945bbf5312225e4f1a39a2cfadb4e5331a066
Nuofan <nuofan.xu@uofthatchery.ca>
2017-05-31 14:07:39 -0400

    added sidebar1

---
+++ with-sidebar(5)/navbar.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+  	var wn = $(window).scrollTop();
+    if(wn > 600){
+    	$(".navbar").css("background","white").css("transition","0.4s ease-in-out ");
+		$(".navbar-default .navbar-nav > li > a ").css("color","black");
+		$(".navbar-default .navbar-nav > li > a ").css("opacity",".6");
+    }
+    else{
+    	$(".navbar").css("background","transparent").css("transition","0.4s ease-in-out ");
+		$(".navbar-default .navbar-nav > li > a ").css("color","#fcf8e3");
+		$(".navbar-default .navbar-nav > li > a ").css("opacity","1");
+    }
+  });
+});
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
