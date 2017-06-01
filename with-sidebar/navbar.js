8b72e3879a65bb493dde4b48aba634ef2bfe5232
Nuofan <nuofan.xu@uofthatchery.ca>
2017-05-31 23:10:49 -0400

    attempt to fix velocity color change after scrolling

--- with-sidebar/navbar.js
+++ with-sidebar/navbar.js
@@ -5,11 +5,15 @@ $(document).ready(function(){
     	$("#myTopnav").css("background","white").css("transition","0.4s ease-in-out ");
 		$(".navbar-default .navbar-nav > li > a ").css("color","black");
 		$(".navbar-default .navbar-nav > li > a ").css("opacity",".6");
+		$(".nav-btn").css("color","black");
+		$(".nav-btn").css("opacity",".9");
     }
     else{
     	$("#myTopnav").css("background","transparent").css("transition","0.4s ease-in-out ");
 		$(".navbar-default .navbar-nav > li > a ").css("color","#fcf8e3");
 		$(".navbar-default .navbar-nav > li > a ").css("opacity","1");
+		$(".nav-btn").css("color","white");
+		$(".nav-btn").css("opacity",".9");
     }
   });
 });
